d245b810595da5b72f529af8c08722a71f639460
Vishal <vishalpal1501@gmail.com>
2023-11-18 14:38:35 +0530

    Another way using usestate in our code and how to avoid number of line in code

--- src/components/indexC.js
+++ src/components/indexC.js
@@ -3,8 +3,9 @@ import React from 'react';
 import './styles/style.css';
 
 function IndexC() {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  //   const [title, setTitle] = useState('');
+  //   const [content, setContent] = useState('');
+  const [formData, setFormData] = useState({ title: '', content: '' });
   const [blogs, setBlogs] = useState([]);
   //   let blog = [
   //     {
@@ -14,11 +15,9 @@ function IndexC() {
   //   ];
   function handleSubmit(e) {
     e.preventDefault();
-    setBlogs([{ title, content }]);
-    setBlogs([{ title, content }, ...blogs]);
-    setTitle('');
-    setContent('');
-    console.log(blogs);
+    // setBlogs([{ title, content }]);
+    setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+    setFormData({ title: '', content: '' });
   }
   return (
     <>
@@ -31,8 +30,10 @@ function IndexC() {
             type="text"
             name="title"
             placeholder="Enter your title here"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={formData.title}
+            onChange={(e) =>
+              setFormData({ title: e.target.value, content: formData.content })
+            }
           />
           <hr />
           <label htmlFor="content">Content</label>
@@ -41,8 +42,8 @@ function IndexC() {
             name="content"
             rows={4}
             cols={30}
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={formData.content}
+            onChange={(e) => setFormData({title:formData.title, content: e.target.value })}
           ></textarea>
           <hr />
           <button>Add</button>
