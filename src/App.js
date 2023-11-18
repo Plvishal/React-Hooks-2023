f3611571f20e4dfe461d55f393ade31c565d317f
Vishal <vishalpal1501@gmail.com>
2023-11-18 21:37:44 +0530

    Setting for the blogging app and learn about the firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-// import IndexC from './components/indexC';
-import LoginIndex from './login/LoginIndex';
+import IndexC from './components/indexC';
+
 function App() {
   return (
     <div className="App">
-      {/* <IndexC /> */}
-      <LoginIndex />
+      <IndexC />
+      {/* <LoginIndex /> */}
     </div>
   );
 }
